6f028ef2ee1d9daceb81ab270feae3f34173edb9
Vincent Prouillet <vincent@wearewizards.io>
2018-07-31 13:59:42 +0200

    Add template.toml validate command. Fix #6

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,4 @@
-use clap::{App, Arg};
+use clap::{App, Arg, SubCommand, AppSettings};
 
 
 pub fn build_cli() -> App<'static, 'static> {
@@ -6,6 +6,7 @@ pub fn build_cli() -> App<'static, 'static> {
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
+        .setting(AppSettings::SubcommandsNegateReqs)
         .arg(
             Arg::with_name("template")
                 .required(true)
@@ -18,5 +19,13 @@ pub fn build_cli() -> App<'static, 'static> {
                 .takes_value(true)
                 .help("Where to output the project: defaults to the current directory")
         )
-        .subcommands(vec![])
+        .subcommands(vec![
+            SubCommand::with_name("validate")
+                .about("Validates that a template.toml is valid")
+                .arg(
+                    Arg::with_name("path")
+                        .required(true)
+                        .help("The path to the template.toml")
+                ),
+        ])
 }
