3ac347a629262b737fcfc9925882ba9c826cd621
chevdor <chevdor@gmail.com>
2021-06-30 12:08:07 +0200

    Cleanup

--- src/prompt.rs
+++ src/prompt.rs
@@ -1,8 +1,8 @@
 use std::io::{self, BufRead, Write};
 
-use regex::Regex;
 use crate::errors::{new_error, ErrorKind, Result};
 use crate::terminal;
+use regex::Regex;
 
 /// Wait for user input and return what they typed
 fn read_line() -> Result<String> {
