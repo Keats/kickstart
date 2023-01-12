48f58c3e120b1ee5ab49fc7ed0238ec415049673
Chevdor <chevdor@gmail.com>
2023-01-12 11:53:50 +0100

    WIP

--- src/bin/kickstart.rs
+++ src/bin/kickstart.rs
@@ -2,45 +2,46 @@ use std::env;
 use std::error::Error;
 use std::path::Path;
 
-use clap::{crate_authors, crate_description, crate_version, App, AppSettings, Arg, SubCommand};
+use clap::{arg, command, Command};
 
 use kickstart::generation::Template;
 use kickstart::terminal;
 use kickstart::validate::validate_file;
 
-pub fn build_cli() -> App<'static, 'static> {
-    App::new("kickstart")
-        .version(crate_version!())
-        .author(crate_authors!())
-        .about(crate_description!())
-        .setting(AppSettings::SubcommandsNegateReqs)
+pub fn build_cli() -> Command {
+    command!()
+        .arg(arg!("kickstart"))
+        // .about(crate_description!())
+        // .setting(AppSettings::SubcommandsNegateReqs)
         .arg(
-            Arg::with_name("template")
+            arg!([name] "template")
                 .required(true)
                 .help("Template to use: a local path or a HTTP url pointing to a Git repository"),
         )
         .arg(
-            Arg::with_name("output-dir")
-                .short("o")
+            arg!([name] "output-dir")
+                .short('o')
                 .long("output-dir")
-                .takes_value(true)
+                .num_args(1)
                 .help("Where to output the project: defaults to the current directory"),
         )
         .arg(
-            Arg::with_name("sub-dir")
-                .short("s")
+            arg!([name]  "sub-dir")
+                .short('s')
                 .long("sub-dir")
-                .takes_value(true)
+                .num_args(1)
                 .help("A subdirectory of the chosen template to use, to allow nested templates."),
         )
         .arg(
-            Arg::with_name("no-input")
+            arg!([name] "no-input")
                 .long("no-input")
                 .help("Do not prompt for parameters and only use the defaults from template.toml"),
         )
-        .subcommands(vec![SubCommand::with_name("validate")
-            .about("Validates that a template.toml is valid")
-            .arg(Arg::with_name("path").required(true).help("The path to the template.toml"))])
+        .subcommand(
+            Command::new("validate")
+                .about("Validates that a template.toml is valid")
+                .arg(arg!([name] "path").required(true).help("The path to the template.toml")),
+        )
 }
 
 fn bail(e: &dyn Error) -> ! {
@@ -57,8 +58,8 @@ fn main() {
     let matches = build_cli().get_matches();
 
     match matches.subcommand() {
-        ("validate", Some(matches)) => {
-            let errs = match validate_file(matches.value_of("path").unwrap()) {
+        Some(("validate", sub_matches)) => {
+            let errs = match validate_file(sub_matches.get_one::<String>("path").unwrap()) {
                 Ok(e) => e,
                 Err(e) => bail(&e),
             };
@@ -75,15 +76,15 @@ fn main() {
         }
         _ => {
             // The actual generation call
-            let template_path = matches.value_of("template").unwrap();
+            let template_path = matches.get_one::<String>("template").unwrap();
             let output_dir = matches
-                .value_of("output-dir")
+                .get_one::<String>("output-dir")
                 .map(|p| Path::new(p).to_path_buf())
                 .unwrap_or_else(|| env::current_dir().unwrap());
-            let no_input = matches.is_present("no-input");
-            let sub_dir = matches.value_of("sub-dir");
+            let no_input = matches.get_one::<String>("no-input").is_some();
+            let sub_dir = matches.get_one::<String>("sub-dir");
 
-            let template = match Template::from_input(template_path, sub_dir) {
+            let template = match Template::from_input(template_path, sub_dir.map(|x| &**x)) {
                 Ok(t) => t,
                 Err(e) => bail(&e),
             };
